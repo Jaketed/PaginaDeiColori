0cc7518db291073fbd1b2c7690e18646fcefe088
Jaketed <michele.pizze97@gmail.com>
2019-10-03 15:59:31 +0200

    Inizializzato il mio primo Git repo <3

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Ciao Kelly (Lelly)!";
+
+
+?>
+
+<html>
+
+
+
+
+</html>
---
+++ pagina_dei_colori.php
@@ -0,0 +1,75 @@
+<?php
+  //  require "controller.php";
+
+  //  $color = getAllColors();
+
+    // $userLoggedIn = getLoggedInUser();
+
+   // if ($userLoggedIn != null) {
+       //$colori = [
+         //  "code" = "#fffff"
+       //]
+   //}
+
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Choose Your Color</title> 
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <h1 style="font-family:elephant;">Scegli il TUO colore</h1>
+       <div id="LogIn">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+       </div>
+        <div id="colori_caldi">
+            <h4>Colori caldi</h4>
+        </div>
+            <div id="contenitore_caldi">
+                <div id="rosso_scuro">
+                    <a href="rosso_scuro.html"><h5>Rosso scuro</h5></a>
+                </div>
+                <div id="rosso">
+                    <a href="rosso.html"><h5>Rosso</h5></a>
+                </div>
+                <div id="arancio">
+                    <a href="arancio.html"><h5>Arancione</h5></a>
+                </div>
+                <div id="giallo_ocra">
+                    <a href="giallo_ocra.html"><h5>Giallo ocra</h5></a>
+                </div>
+                <div id="giallo_canarino">
+                    <a href="giallo_canarino.html"><h5>Giallo</h5></a>
+                </div>
+            </div>
+            <div id="colori_freddi">
+                <h4>Colori freddi</h4>
+            </div>
+            <div id="contenitore_freddi">
+                <div id="blu_scuro">
+                    <a href="blu_scuro.html"><h5>Blu scuro</h5></a>
+                </div>
+                <div id="azzurro">
+                    <a href="azzurro.html"><h5>Azzurro</h5></a>
+                </div>
+                <div id="ciano">
+                    <a href="ciano.html"><h5>Ciano</h5></a>
+                </div>
+                <div id="lime">
+                    <a href="lime.html"><h5>Lime</h5></a>
+                </div>
+                <div id="verde">
+                    <a href="verde.html"><h5>Verde</h5></a>
+                </div>
+            </div>
+</body>
+</html>
